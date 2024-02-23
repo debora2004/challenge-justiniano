4b94b1f6efa5a5a5020b12fd1328c7cbb55a91f7
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2024-02-23 15:32:07 -0300

    [FEAT] restaurant model - service - controller

---
+++ src/models/restaurant.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const RestaurantSchema = new mongoose.Schema({
+    id: {
+    type: Number,
+    autoincrement: true,
+    },
+    name: {
+    type: String,
+    required: true,
+    },
+    address: {
+    type: String,
+    required: true,
+    },
+    operating_hours: {
+    type: Date,
+    required: true,
+    },
+    photo: {
+    type: String,
+    required: true,
+    },
+});
+
+const Restaurant = mongoose.model('Restaurant', RestaurantSchema);
+module.exports = Restaurant;

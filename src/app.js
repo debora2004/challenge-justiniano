718bb221b62132be6b5cd9a3908fe017c46c11cd
AbigailJustiniano <70248439+debora2004@users.noreply.github.com>
2024-02-24 08:58:27 -0300

    [FIX] Agregado de endopoints para product y FIX en restaurant nomenclatura

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const restaurantController = require('./controllers/restaurantController');
+const productController = require('./controllers/productController');
 const multer = require('multer');
 require('./database');
 
 const storage = multer.diskStorage({
-  destination: '/src/uploads/',
+  destination: './src/uploads/',
   filename: (req, file, cb) => {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
     cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.mimetype.split('/')[1]);
@@ -19,10 +20,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 
+//Restaurant routes
 app.post('/restaurant', upload.single('photo'), restaurantController.registerRestaurant); 
 app.put('/restaurant/:id', upload.single('photo'), restaurantController.updateRestaurant);
-app.get('/restaurant',   restaurantController.listAllRestaurant);
+app.get('/restaurant',   restaurantController.listAllRestaurants);
 app.get('/restaurant/:id', restaurantController.findByIdRestaurant);
 app.delete('/restaurant/:id', restaurantController.deleteRestaurant);
+
+//Product routes
+app.post('/product/:id', upload.single('photo'), productController.registerProduct ); 
+app.put('/product/:id', upload.single('photo'), productController.updateProduct);
+app.get('/product',   productController.listAllProducts);
+app.get('/product/:id', productController.findByIdProduct);
+app.delete('/product/:id', productController.deleteProduct);
 
 module.exports = app;
